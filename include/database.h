4a44f6e49db03b29621c157774313457fb58c1b2
Milos Zivlak <zivlakmilos@gmail.com>
2017-12-19 22:11:25 +0100

    Basic database schema

---
+++ include/database.h
@@ -0,0 +1,14 @@
+#ifndef _DATABASE_H_
+#define _DATABASE_H_
+
+#include <QSqlDatabase>
+
+namespace Database
+{
+    QSqlDatabase loadDatabase(void);
+    QSqlDatabase getDatabase(void);
+    bool isSchemaExists(const QSqlDatabase &database);
+    void createSchema(const QSqlDatabase &database);
+}
+
+#endif // _DATABASE_H_
---
+++ src/database.cpp
@@ -0,0 +1,68 @@
+#include "database.h"
+
+#include <QDesktopServices>
+#include <QSqlQuery>
+#include <QVariant>
+
+QSqlDatabase Database::loadDatabase(void)
+{
+    QString path = QDesktopServices::storageLocation(QDesktopServices::DataLocation) + "/database.db";
+
+    QSqlDatabase database = QSqlDatabase::addDatabase("SQLITE");
+    database.setDatabaseName(path);
+    database.open();
+    return database;
+}
+
+QSqlDatabase Database::getDatabase(void)
+{
+    return QSqlDatabase::database();
+}
+
+bool Database::isSchemaExists(const QSqlDatabase &database)
+{
+    QString sql = "SELECT COUNT(*) FROM sqlite_master WHERE type='table'";
+    QSqlQuery query(sql, database);
+
+    if(query.exec && query.next())
+    {
+        if(query.value(0).toInt() < 4)
+            return true;
+    }
+
+    return false;
+}
+
+void Database::createSchema(const QSqlDatabase &database)
+{
+    QString sql = "CREATE TABLE IF NOT EXISTS `appeals` ("
+                  "    `id`    INTEGER,"
+                  "    `text`  TEXT,"
+                  "    PRIMARY KEY(`id`)"
+                  ");"
+                  "CREATE TABLE IF NOT EXISTS `me` ("
+                  "    `id`    INTEGER,"
+                  "    `email` TEXT,"
+                  "    `name`  TEXT,"
+                  "    `surname`   TEXT,"
+                  "    `signiture` TEXT,"
+                  "    PRIMARY KEY(`id`)"
+                  ");"
+                  "CREATE TABLE IF NOT EXISTS `messages` ("
+                  "`id`    INTEGER,"
+                  "`message`   TEXT,"
+                  "PRIMARY KEY(`id`)"
+                  ");"
+                  "CREATE TABLE IF NOT EXISTS `peoples` ("
+                  "    `id`    INTEGER,"
+                  "    `email` TEXT,"
+                  "    `name`  TEXT,"
+                  "    `surname`   TEXT,"
+                  "    `prefix`    TEXT,"
+                  "    `appeal`    INTEGER,"
+                  "    `sex`   INTEGER,"
+                  "    PRIMARY KEY(`id`)"
+                  ");";
+    QSqlQuery query(sql, database);
+    query.exec();
+}
